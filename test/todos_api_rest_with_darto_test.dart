ae734324de52f5758c7a230eb759a3d97d198aed
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2025-04-24 12:56:26 -0300

    init api

---
+++ test/todos_api_rest_with_darto_test.dart
@@ -0,0 +1,8 @@
+import 'package:todos_api_rest_with_darto/todos_api_rest_with_darto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
