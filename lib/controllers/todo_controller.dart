d053ebc63006ecb357195fe150c971fe387ea0ba
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2025-04-24 13:38:55 -0300

    validation api

--- lib/controllers/todo_controller.dart
+++ lib/controllers/todo_controller.dart
@@ -1,4 +1,5 @@
 import 'package:darto/darto.dart';
+import 'package:zard/zard.dart';
 
 class TodoController {
   int _idCounter = 1;
@@ -9,28 +10,55 @@ class TodoController {
   }
 
   Future<void> create(Request req, Response res) async {
-    final body = await req.body;
+    final todoBodySchema = z.map({
+      'title': z.string().min(3),
+      'completed': z.bool().optional(),
+    });
 
-    final todo = {'id': _idCounter++, ...body};
-    _todos.add(todo);
+    try {
+      final todo = todoBodySchema.parse(await req.body)!;
+      _todos.add({...todo, 'id': _idCounter++});
 
-    return res.status(201).end();
+      return res.status(201).end();
+    } catch (e) {
+      return res.status(500).json({'Errors': todoBodySchema.getErrors()});
+    }
   }
 
   void update(Request req, Response res) async {
-    final id = int.parse(req.params['id'] ?? '');
-    final body = await req.body;
+    final paramSchema = z.coerce.int();
+    final todoBodySchema = z.map({
+      'title': z.string().min(3),
+      'completed': z.bool().optional(),
+    });
 
-    final index = _todos.indexWhere((todo) => todo['id'] == id);
+    try {
+      final id = paramSchema.parse(req.params['id']);
 
-    if (index == -1) return res.status(404).send('Todo not found!');
-    _todos[index] = {'id': id, ...body};
-    return res.status(200).json(_todos[index]);
+      final body = todoBodySchema.parse(await req.body)!;
+
+      final index = _todos.indexWhere((todo) => todo['id'] == id);
+
+      if (index == -1) return res.status(404).send('Todo not found!');
+      _todos[index] = {'id': id, ...body};
+      return res.status(200).json(_todos[index]);
+    } catch (e) {
+      return res.status(500).json({
+        'Errors': [...todoBodySchema.getErrors(), ...paramSchema.getErrors()],
+      });
+    }
   }
 
   void delete(Request req, Response res) {
-    final id = int.parse(req.params['id'] ?? '');
-    _todos.removeWhere((todo) => todo['id'] == id);
+    final paramSchema = z.coerce.int();
+
+    try {
+      final id = paramSchema.parse(req.params['id'] ?? '');
+
+      _todos.removeWhere((todo) => todo['id'] == id);
+    } catch (e) {
+      return res.status(500).json({'Errors': paramSchema.getErrors()});
+    }
 
     return res.status(204).end();
   }
